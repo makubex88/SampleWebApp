7f8929c80b12eca3b4d487df48dafb39eacae36e
jeric <jericworks@gmail.com>
2017-08-14 15:34:38 +0800

    test

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication2.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "test 1234.";
 
             return View();
         }
